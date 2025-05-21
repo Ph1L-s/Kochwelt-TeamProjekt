e44549d290bc123aa9319c38389e48e48d40c915
knostijr <christophkonst@icloud.com>
2025-05-21 13:20:32 +0200

    kontakt JS
    nachricht absenden möglich

--- kontakt.js
+++ kontakt.js
@@ -1,4 +1,16 @@
-document.getElementById('alert').addEventListener('click', function() {
-    alert('Deine Nachricht wurde abgeschickt!');
+function sendMail(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
 
-});
+    fetch("https://formspree.io/f/xrbqdnpl", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./send_mail.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
